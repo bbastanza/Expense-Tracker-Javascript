238a69e6e2cbccc377765b92846861555d442910
bbastanza <b.bastanza@gmail.com>
2020-08-18 14:52:46 -0700

    commit refactor/cleanup

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 # Vanilla JS Expense Tracker
 
->Expense Tracker app written in pure JS, HTML & CSS
+> Expense Tracker app written in pure JS, HTML & CSS
 
 ![js](MDimages/javascript.png) ![html](MDimages/html.png) ![css](MDimages/css.png)
 
 Check out the app [here](https://bbastanza.github.io/expense-vanilla/)
 
- ![Screenshot](MDimages/screenshot.png)
-
-
+![Screenshot](MDimages/screenshot.png)
 
 ## Summary
+
 I made this app to further my knowledge of JS, HTML and CSS.
 
 Building this app I learned more about using forms in HTML to edit the DOM and how to edit HTML tables through javascript.
 
 I learned a lot during this project and it really helped to cement some concepts I was previously working on while adding a few more minor concepts on top.
 
-
 ## Author
+
 Brian Bastanza- Full Stack Web Developer
--- index.js
+++ index.js
@@ -1,42 +1,62 @@
 //
-// *****************Project 4- Vanilla JS Expense Tracker******************************//
-//
-//
-// displays the table on start
 document.addEventListener("DOMContentLoaded", function () {
     displayExpensesOnTable();
+
+    const inputs = document.getElementsByClassName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        const input = inputs[i];
+        input.addEventListener("keypress", function (e) {
+            switch (e.key) {
+                case "Enter":
+                    addNewExpense();
+                default:
+                    break;
+            }
+        });
+    }
 });
 
-// event listener for remove all expenses button
-document.getElementById("remove-all").addEventListener("click", function () {
-    const expenses = [];
+const removeAll = document.getElementById("remove-all");
+const submitButton = document.getElementById("submit");
+const clearButton = document.getElementById("clear");
+const table = document.getElementById("table");
 
+removeAll.addEventListener("click", function () {
     document.getElementById("table").innerHTML = "";
 
-    window.localStorage.setItem('expenses', JSON.stringify(expenses));
+    window.localStorage.setItem("expenses", JSON.stringify([]));
 
     displayExpensesOnTable();
 });
 
-// function to add expences
-document.getElementById("submit").addEventListener("click", function (e) {
+submitButton.addEventListener("click", function (e) {
     e.preventDefault();
     addNewExpense();
 });
 
-// for loop to loop through inputs with the class name "input" to add enter key functionality
-for (let i = 0; i < document.getElementsByClassName('input').length; i++) {
-    const input = document.getElementsByClassName('input')[i];
-    input.addEventListener('keypress', function (e) {
-        if (e.key === "Enter") {
-            addNewExpense();
-        };
-    });
-};
+clearButton.addEventListener("click", function () {
+    document.getElementById("form").reset();
+});
+
+table.addEventListener("click", function (e) {
+    const item = e.target;
+
+    let expenses = getSavedExpenses();
+    if (item.className === "delete") {
+        for (let i = 0; i < expenses.length; i++) {
+            const expense = expenses[i];
+
+            if (expense.id == item.id) {
+                expenses.splice(i, 1);
+            }
+        }
+        table.removeChild(item.parentNode.parentNode);
+    }
+
+    window.localStorage.setItem("expenses", JSON.stringify(expenses));
+});
 
 function addNewExpense() {
-    let expenses = getSavedExpenses();
-
     const newExpense = {
         id: new Date().getTime() * Math.random(),
         amount: parseFloat(document.getElementById("amount").value).toFixed(2),
@@ -45,14 +65,12 @@ function addNewExpense() {
         date: document.getElementById("date").value,
     };
 
-
-    const paymentTypes = document.getElementsByName("payment-type")
+    const paymentTypes = document.getElementsByName("payment-type");
     for (let i = 0; i < paymentTypes.length; i++) {
         if (paymentTypes[i].checked) {
             newExpense.type = paymentTypes[i].value;
-        };
-    };
-
+        }
+    }
 
     if ((newExpense.place === "" || newExpense.date === "") && isNaN(newExpense.amount) === true) {
         alert("Please fill out all fields & amount must be a number");
@@ -63,36 +81,40 @@ function addNewExpense() {
     } else if (isNaN(newExpense.amount) === true) {
         alert("Amount must be a number");
         return;
-    };
+    }
 
-    newExpense.amount = `$${newExpense.amount}`
+    newExpense.amount = `$${newExpense.amount}`;
 
+    let expenses = getSavedExpenses();
     expenses.push(newExpense);
     document.getElementById("table").innerHTML = "";
-    window.localStorage.setItem('expenses', JSON.stringify(expenses));
+    window.localStorage.setItem("expenses", JSON.stringify(expenses));
     displayExpensesOnTable();
-};
+}
 
-// funciton to clear the form
-document.getElementById("clear").addEventListener("click", function (e) {
-    document.getElementById("form").reset();
-});
-
-
-// function to get saved expenses from local storage
 function getSavedExpenses() {
-    let savedExpenses = JSON.parse(window.localStorage.getItem('expenses'));
-    if (savedExpenses === null) {
-        savedExpenses = [];
-    };
-    return savedExpenses;
-};
+    return JSON.parse(window.localStorage.getItem("expenses")) || [];
+}
 
-
-// function to display all expenses on the table from local storage
 function displayExpensesOnTable() {
-    let expenses = getSavedExpenses();
     const htmlTable = document.getElementById("table");
+
+    const thRow = createTableHeader();
+    htmlTable.appendChild(thRow);
+    document.getElementById("form").reset();
+
+    let expenses = getSavedExpenses();
+    for (let i = 0; i < expenses.length; i++) {
+        const expense = expenses[i];
+
+        if (expense !== null) {
+            tableRow = createDataRow(expense);
+            htmlTable.appendChild(tableRow);
+        }
+    }
+}
+
+function createTableHeader() {
     const thRow = document.createElement("tr");
     const typeTh = document.createElement("th");
     const dateTh = document.createElement("th");
@@ -109,56 +131,29 @@ function displayExpensesOnTable() {
     thRow.appendChild(amountTh);
     thRow.appendChild(placeTh);
     thRow.appendChild(removeTh);
-    htmlTable.appendChild(thRow);
-    document.getElementById("form").reset();
+    return thRow;
+}
 
-    for (let i = 0; i < expenses.length; i++) {
-        const expense = expenses[i];
-
-        if (expense !== null) {
-            const tableRow = document.createElement("tr");
-            const typeTd = document.createElement("td");
-            const dateTd = document.createElement("td");
-            const amountTd = document.createElement("td");
-            const placeTd = document.createElement("td");
-            const removeTd = document.createElement("td");
-            const removeButton = document.createElement("button");
-            removeButton.id = expense.id;
-            removeButton.className = "delete";
-            removeButton.innerHTML = "X";
-            removeTd.appendChild(removeButton);
-            typeTd.textContent = expense.type;
-            dateTd.textContent = expense.date;
-            amountTd.textContent = expense.amount;
-            placeTd.textContent = expense.place;
-            tableRow.appendChild(typeTd);
-            tableRow.appendChild(dateTd);
-            tableRow.appendChild(amountTd);
-            tableRow.appendChild(placeTd);
-            tableRow.appendChild(removeTd);
-            htmlTable.appendChild(tableRow);
-        };
-    };
-};
-
-// function to delete a expense
-const table = document.getElementById("table");
-table.addEventListener("click", function (e) {
-    let expenses = getSavedExpenses();
-
-    const item = e.target;
-
-    for (let i = 0; i < expenses.length; i++) {
-        const expense = expenses[i];
-
-        if (expense.id == item.id) {
-            expenses.splice(i, 1);
-        };
-    };
-
-    if (item.className === "delete") {
-        table.removeChild(item.parentNode.parentNode);
-    };
-
-    window.localStorage.setItem('expenses', JSON.stringify(expenses));
-});
+function createDataRow(expense) {
+    const tableRow = document.createElement("tr");
+    const typeTd = document.createElement("td");
+    const dateTd = document.createElement("td");
+    const amountTd = document.createElement("td");
+    const placeTd = document.createElement("td");
+    const removeTd = document.createElement("td");
+    const removeButton = document.createElement("button");
+    removeButton.id = expense.id;
+    removeButton.className = "delete";
+    removeButton.innerHTML = "X";
+    removeTd.appendChild(removeButton);
+    typeTd.textContent = expense.type;
+    dateTd.textContent = expense.date;
+    amountTd.textContent = expense.amount;
+    placeTd.textContent = expense.place;
+    tableRow.appendChild(typeTd);
+    tableRow.appendChild(dateTd);
+    tableRow.appendChild(amountTd);
+    tableRow.appendChild(placeTd);
+    tableRow.appendChild(removeTd);
+    return tableRow;
+}
